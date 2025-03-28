c28948ba168c54870aeeb4d8850990c604e8e00e
Vika Filipenko <vika.fili.viku1207@gmail.com>
2025-03-28 12:12:18 +0300

    mseilfjseoifjoiesdjv

--- api/views.py
+++ api/views.py
@@ -1,19 +1,31 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.exceptions import MethodNotAllowed
 
 import api.serializers as sl
 from api.permissions import IsAuthorOrReadOnly
 from posts.models import Group, Post
+from .serializers import GroupSerializer
 
 API_PERMISSIONS = [IsAuthenticated, IsAuthorOrReadOnly]
 
 
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+class GroupViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Group."""
 
     queryset = Group.objects.all()
-    serializer_class = sl.GroupSerializer
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PUT method is not allowed.')
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PATCH method is not allowed.')
+    
+    def create(self, request, *args, **kwargs):
+        raise MethodNotAllowed('POST method is not allowed for creating groups.')
 
 
 class PostViewSet(viewsets.ModelViewSet):
